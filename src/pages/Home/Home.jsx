d5f192586ef1d4ecb3b8ebf7a5e7a3266b94ffb7
Ihor Statsenko <statsenkoin@gmail.com>
2023-04-21 22:35:06 +0300

    add link to tweet page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,12 @@
-import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function Home() {
-  return <>Home</>;
+  const location = useLocation();
+  return (
+    <>
+      <Link to="/tweets" state={{ from: location }}>
+        Home
+      </Link>
+    </>
+  );
 }
