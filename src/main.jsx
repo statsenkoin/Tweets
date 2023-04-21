364b97492181533231a8ea18f7159082fc826bba
Ihor Statsenko <statsenkoin@gmail.com>
2023-04-21 22:55:51 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Tweets">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
