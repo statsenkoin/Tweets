7651327a0f68e0763afe09688d3a8416a667e5fc
statsenkoin <statsenkoin@users.noreply.github.com>
2023-04-21 19:56:34 +0000

    deploy: 364b97492181533231a8ea18f7159082fc826bba

--- assets/Home-49f4d55b.js
+++
@@ -1,1 +0,0 @@
-import{u as e,j as t,L as s}from"./index-5685b242.js";function r(){const o=e();return t.jsx(t.Fragment,{children:t.jsx(s,{to:"/tweets",state:{from:o},children:"Home"})})}export{r as default};
