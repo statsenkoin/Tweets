255375966e3e10e00a6e4ad6dbb5468863a56f47
Ihor Statsenko <statsenkoin@gmail.com>
2023-04-21 20:00:26 +0300

    Create App.jsx

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import './App.css';
+
+export function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>App</h1>
+    </>
+  );
+}
