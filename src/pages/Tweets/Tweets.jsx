7d6bd86e1bb906162f0a300ea00b47e891bbe4b0
Ihor Statsenko <statsenkoin@gmail.com>
2023-04-22 15:18:41 +0300

    Update Tweets.jsx

--- src/pages/Tweets/Tweets.jsx
+++ src/pages/Tweets/Tweets.jsx
@@ -1,12 +1,16 @@
 import { Link, useLocation } from 'react-router-dom';
 
 export default function Tweets() {
-  const location = useLocation();
-  const backLinkHref = location.state?.from ?? '/';
+  // const location = useLocation();
+  // const backLinkHref = location.state?.from ?? '/';
   return (
     <>
       Tweets
-      <Link to={backLinkHref}>Back to products</Link>
+      <Link
+        // to={backLinkHref}
+        to="/">
+        Back to products
+      </Link>
     </>
   );
 }
