3aea1ede42ae6b92549907afb98879fcdc5e16ef
Ihor Statsenko <statsenkoin@gmail.com>
2023-04-22 15:32:29 +0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   const location = useLocation();
   return (
     <>
-      <Link to="/tweets" state={{ from: location }}>
+      <Link to="/alltweets" state={{ from: location }}>
         Home
       </Link>
     </>
